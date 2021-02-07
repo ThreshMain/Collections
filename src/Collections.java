b3131c223e8478c68a0766f4e213401b3b80a9ef
ThreshMain <martinkos007@gmail.com>
2021-02-07 16:41:34 +0100

    Thread safe testing

--- src/Collections.java
+++ src/Collections.java
@@ -2,14 +2,32 @@ import java.util.*;
 import java.util.function.Function;
 
 public class Collections {
-    public ArrayList<String> arrayList;
-    public LinkedList<String> linkedList;
-    public TreeSet<String> treeSet;
-    public HashMap<String, String> hashMap;
-    public TreeMap<String, String> treeMap;
-    public PriorityQueue<String> priorityQueue;
-    public ArrayDeque<String> arrayDeque;
+    public static ArrayList<String> arrayList;
+    public static LinkedList<String> linkedList;
+    public static TreeSet<String> treeSet;
+    public static HashMap<String, String> hashMap;
+    public static TreeMap<String, String> treeMap;
+    public static PriorityQueue<String> priorityQueue;
+    public static ArrayDeque<String> arrayDeque;
+
+    private static int randomisingIndex = 0;
+    private static int randomisingIndex2 = 0;
+
     private final String[] testingArray = new String[]{"bcda", "cdab", "abcd"};
+
+
+    private static int getRandomisingIndex() {
+        return ++randomisingIndex;
+    }
+
+    private static int getRandomisingIndex2() {
+        return ++randomisingIndex;
+    }
+
+    private static void resetRandomissingIndexes() {
+        randomisingIndex = 0;
+        randomisingIndex2 = 0;
+    }
 
     public Collections() {
         arrayList = new ArrayList<>();
@@ -36,11 +54,22 @@ public class Collections {
         }
     }
 
+    private static void emptyAll() {
+        arrayList.clear();
+        linkedList.clear();
+        treeSet.clear();
+        hashMap.clear();
+        treeMap.clear();
+        priorityQueue.clear();
+        arrayDeque.clear();
+    }
+
     public void runAllTests() {
         testOrder();
         testSorted();
         testSameValues();
         testNull();
+        testThreadSafe();
     }
 
     /**
@@ -83,8 +112,8 @@ public class Collections {
 
     private void compareCollectionToArray(String[] copy) {
 
-        ArrayList<String> saveOfPriorityQueue=new ArrayList<>();
-        while(priorityQueue.size()>0){
+        ArrayList<String> saveOfPriorityQueue = new ArrayList<>();
+        while (priorityQueue.size() > 0) {
             saveOfPriorityQueue.add(priorityQueue.poll());
         }
 
@@ -123,9 +152,7 @@ public class Collections {
                 ConsoleColors.printRed("arrayDeque - fail:" + i + ", " + y.toString());
             }
         }
-        for (String s : saveOfPriorityQueue) {
-            priorityQueue.add(s);
-        }
+        priorityQueue.addAll(saveOfPriorityQueue);
     }
 
     /***
@@ -324,6 +351,80 @@ public class Collections {
         }
         if (arrayDeque.size() == sizeBefore) {
             ConsoleColors.printRed("arrayDeque - fail");
+        }
+    }
+
+    public static void testThreadSafe() {
+        ConsoleColors.printBlue("Collections.testThreadSafe");
+        emptyAll();
+        int numberToAdd, size;
+
+        numberToAdd = 10000;
+        testThreadSafe(numberToAdd, () -> Collections.arrayList.add(Collections.getRandomisingIndex() + "TEST"), () -> Collections.arrayList.add(Collections.getRandomisingIndex2() + "TEST"));
+        size = arrayList.size();
+        compareThreadSafeResults(size, numberToAdd * 2, "arrayList");
+
+
+        numberToAdd = 10000;
+        testThreadSafe(numberToAdd, () -> Collections.linkedList.add(Collections.getRandomisingIndex() + "TEST"), () -> Collections.linkedList.add(Collections.getRandomisingIndex2() + "TEST"));
+        size = linkedList.size();
+        compareThreadSafeResults(size, numberToAdd * 2, "linkedList");
+
+        numberToAdd = 500;
+        testThreadSafe(numberToAdd, () -> Collections.treeSet.add(Collections.getRandomisingIndex() + "TEST"), () -> Collections.treeSet.add(Collections.getRandomisingIndex2() + "TEST"));
+        size = treeSet.size();
+        compareThreadSafeResults(size, numberToAdd * 2, "treeSet");
+
+        numberToAdd = 10000;
+        testThreadSafe(numberToAdd, () -> Collections.hashMap.put(Collections.getRandomisingIndex() + "TEST", Collections.getRandomisingIndex() + "TEST"), () -> Collections.hashMap.put(Collections.getRandomisingIndex2() + "TEST", Collections.getRandomisingIndex2() + "TEST"));
+        size = hashMap.size();
+        compareThreadSafeResults(size, numberToAdd * 2, "hashMap");
+
+        numberToAdd = 500;
+        testThreadSafe(numberToAdd, () -> Collections.treeMap.put(Collections.getRandomisingIndex() + "TEST", Collections.getRandomisingIndex() + "TEST"), () -> Collections.treeMap.put(Collections.getRandomisingIndex2() + "TEST", Collections.getRandomisingIndex2() + "TEST"));
+        size = treeMap.size();
+        compareThreadSafeResults(size, numberToAdd * 2, "treeMap");
+
+        numberToAdd = 500;
+        testThreadSafe(numberToAdd, () -> Collections.priorityQueue.add(Collections.getRandomisingIndex() + "TEST"), () -> Collections.priorityQueue.add(Collections.getRandomisingIndex2() + "TEST"));
+        size = priorityQueue.size();
+        compareThreadSafeResults(size, numberToAdd * 2, "priorityQueue");
+
+        numberToAdd = 10000;
+        testThreadSafe(numberToAdd, () -> Collections.arrayDeque.add(Collections.getRandomisingIndex() + "TEST"), () -> Collections.arrayDeque.add(Collections.getRandomisingIndex2() + "TEST"));
+        size = arrayDeque.size();
+        compareThreadSafeResults(size, numberToAdd * 2, "arrayDeque");
+    }
+
+    public static void compareThreadSafeResults(int x, int y, String msg) {
+        if (x != y) {
+            ConsoleColors.printRed("Thread safe fail " + msg + " (" + x + "," + y * 2 + ")");
+        } else {
+            ConsoleColors.printGreen("Thread safe success " + msg + " (" + x + "," + y * 2 + ")");
+        }
+    }
+
+
+    public static void testThreadSafe(int numberOfTimes, Runnable runnable, Runnable runnable2) {
+        Thread thread = new Thread(() -> repeatRunnable(numberOfTimes, runnable));
+        Thread thread2 = new Thread(() -> repeatRunnable(numberOfTimes, runnable2));
+        thread.start();
+        thread2.start();
+        try {
+            thread.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        resetRandomissingIndexes();
+    }
+
+    public static void repeatRunnable(int numberOfTimes, Runnable runnable) {
+        for (int i = 0; i < numberOfTimes; i++) {
+            try {
+                runnable.run();
+            } catch (Exception ignored) {
+            }
         }
     }
 }
